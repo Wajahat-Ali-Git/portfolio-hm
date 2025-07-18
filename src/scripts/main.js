d84cca53224af967df21407fdf5aa76a99834274
Wajahat  Ali <wajahatali@Devflovvs-MacBook-Pro.local>
2025-07-18 23:58:14 +0500

    feat:added number loading animation and made some UI improvements

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,24 +6,27 @@ window.addEventListener("scroll", function () {
   } else {
     navbar.classList.remove("scrolled");
   }
-  
 });
-
-
 
 // move-up buttom function
 
-window.addEventListener("scroll", function(){
-  const moveUpButton = this.document.getElementById("moveUp-handler")
+window.addEventListener("scroll", function () {
+  const moveUpButton = this.document.getElementById("moveUp-handler");
 
-  if(window.scrollY > 500 ){
-    moveUpButton.classList.add('active');
+  if (window.scrollY > 500) {
+    moveUpButton.classList.add("active");
   } else {
-    moveUpButton.classList.remove('active');
+    moveUpButton.classList.remove("active");
   }
-})
+});
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 1100) {
+    setInterval(intervalFunc, 10);
+  }
+});
+
 // move-up buttom function ends here
- 
 
 const text = document.querySelector(".text-change");
 
@@ -44,3 +47,19 @@ const textLoad = () => {
 textLoad();
 
 setInterval(textLoad, 12000);
+
+// auto number increment section start from here
+
+var count = document.getElementsByClassName("count");
+var inc = [];
+function intervalFunc() {
+  for (let i = 0; i < count.length; i++) {
+    // console.log(count.length);
+    // console.log(i);
+    inc.push(1);
+    if (inc[i] != count[i].getAttribute("max-data")) {
+      inc[i]++;
+    }
+    count[i].innerHTML = inc[i];
+  }
+}
